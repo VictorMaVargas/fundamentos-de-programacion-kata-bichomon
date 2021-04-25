2a9671f29335a01281c8f11051d62c5ffade5b86
VictorMaVargas <victormavargas@gmail.com>
2021-04-25 20:13:35 +0200

    fondo

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
+document.getElementById("gen-1").innerHTML = "Generasión Pokimon";
+
+let pokimon = document.getElementsByClassName("infocard-list infocard-list-pkmn-lg");
+    for(let i = 0; i < pokimon.length; i++){
+        pokimon[0].style.background = "lightblue";
+    }
+
 console.log(document.title);
